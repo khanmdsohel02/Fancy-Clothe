85aa13b2180307024fe3df589f756d4bd19a4002
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-07-06 18:03:47 +0600

    home page done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("first");
+// Navbar JS
+
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const closeIcon = document.querySelector("#close-icon");
+const menuItems = document.querySelector("#menu-items");
+
+hamburgerIcon.addEventListener("click", () => {
+  menuItems.style = "right: 0";
+});
+
+closeIcon.addEventListener("click", () => {
+  menuItems.style = "right: -20rem";
+});
+
+// Navbar JS End
